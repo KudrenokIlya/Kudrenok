ad0c5fc293599dc50d65a0a665019c68c97723ab
KudrenokIlya <112194418+KudrenokIlya@users.noreply.github.com>
2022-10-27 11:15:42 +0300

    Add files via upload

---
+++ _sharp_basic_3/_sharp_basic_3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Homework_2
+{
+    static void Main()
+    {
+        //Task1
+        {
+            int enterNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (enterNumber % 2 == 0 && enterNumber % 5 == 0)
+            {
+                Console.WriteLine("tutti-frutti");
+            }
+
+            else if (enterNumber % 5 == 0)
+            {
+                Console.WriteLine("frutti");
+            }
+
+            else if (enterNumber % 2 == 0)
+
+            {
+                Console.WriteLine("tutti");
+            }
+
+            Console.ReadLine();
+        }
+
+        //Task2
+        {
+            int firstnumber = Convert.ToInt32(Console.ReadLine());
+            int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = firstnumber; i <= secondnumber; i++)
+            {
+
+                if (i % 2 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("tutti-frutti");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("frutti");
+                }
+
+                else if (i % 2 == 0)
+                {
+                    Console.WriteLine("tutti");
+                }
+
+                else
+                {
+                    Console.WriteLine("Сan’t be delived on 2 or 5");
+                }
+
+                Console.ReadLine();
+
+
+            }
+
+            //Task3
+            {
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                if (firstNumber > secondNumber)
+                {
+                    Console.WriteLine($"{firstNumber} is bigger");
+                }
+                else if (secondNumber > firstNumber)
+                {
+                    Console.WriteLine($"{secondNumber} is bigger");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+
+            }
+    }
+}
+}
